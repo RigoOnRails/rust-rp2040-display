62e48793020379d0c9e4f9062f9f49ae1ffcf28e
Rigo <rigothedev@gmail.com>
2023-03-23 01:20:05 -0700

    Do nothing forever

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use rp_pico::entry;
+use defmt::info;
+use defmt_rtt as _;
+use panic_probe as _;
+
+#[entry]
+fn main() -> ! {
+    info!("Program started...");
+
+    loop {}
 }
